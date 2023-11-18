5295daebeb6fd49264ccd707f6e08025cc7b254c
Leonid Burtsev <unoejk@gmail.com>
2023-11-19 01:45:08 +0300

    oversize ready

--- src/js/sidebarsOpening.js
+++ src/js/sidebarsOpening.js
@@ -1,41 +1,60 @@
 'use strict'
 
 export const useSidebarsOpening=()=>{
+
     const sidebarBurger=document.querySelector('.sidebarBurger')
     const sidebarCall=document.querySelector('.sidebarCall')
     const sidebarChat=document.querySelector('.sidebarChat')
+
+    const openBurger=document.querySelectorAll('.openBurger')
+    const openCall=document.querySelectorAll('.openCall')
+    const openChat=document.querySelectorAll('.openChat')
+
+    const closeLeftSidebar=document.querySelectorAll('.closeLeftSidebar')
+    const closeRightSidebar=document.querySelectorAll('.closeRightSidebar')
     const overlay=document.querySelector('.overlay')
 
-    const openBurger=document.querySelector('.openBurger')
-    const openCall=document.querySelector('.openCall')
-    const openChat=document.querySelector('.openChat')
-
-    // const testBtnBurger=document.querySelector('.testBtnBurger')
-    // const testBtnCall=document.querySelector('.testBtnCall')
-    // const testBtnChat=document.querySelector('.testBtnChat')
-    const closeSidebars=document.querySelectorAll('.closeSidebars')
-
-    openBurger.addEventListener('click',()=>{
-        sidebarBurger.classList.add('sidebar--isOpen')
-        overlay.classList.add('overlay--isOpen--forLeftSidebar')
+    //open
+    openBurger.forEach((el,num)=>{
+        el.addEventListener('click',()=>{
+            sidebarBurger.classList.add('sidebar--isOpen')
+            overlay.classList.add('overlay--isOpen--forLeftSidebar')
+        })
     })
-    openCall.addEventListener('click',()=>{
-        sidebarCall.classList.add('sidebar--isOpen')
-        overlay.classList.add('overlay--isOpen--forRightSidebar')
+    openCall.forEach((el,num)=>{
+        el.addEventListener('click',()=>{
+            sidebarCall.classList.add('sidebar--isOpen')
+            overlay.classList.add('overlay--isOpen--forRightSidebar')
+        })
     })
-    openChat.addEventListener('click',()=>{
-        sidebarChat.classList.add('sidebar--isOpen')
-        overlay.classList.add('overlay--isOpen--forRightSidebar')
+    openChat.forEach((el,num)=>{
+        el.addEventListener('click',()=>{
+            sidebarChat.classList.add('sidebar--isOpen')
+            overlay.classList.add('overlay--isOpen--forRightSidebar')
+        })
     })
 
-
-    // const closeSidebars=[testBtnBurger,testBtnCall,testBtnChat,overlay]
-    closeSidebars.forEach((el,num)=>{
+    // close
+    closeLeftSidebar.forEach((el,num)=>{
         el.addEventListener('click',()=>{
             sidebarBurger.classList.remove('sidebar--isOpen')
+            overlay.classList.remove('overlay--isOpen--forLeftSidebar')
+        })
+    })
+    closeRightSidebar.forEach((el,num)=>{
+        el.addEventListener('click',()=>{
             sidebarCall.classList.remove('sidebar--isOpen')
             sidebarChat.classList.remove('sidebar--isOpen')
-            overlay.classList.remove('overlay--isOpen--forLeftSidebar', 'overlay--isOpen--forRightSidebar')
+            overlay.classList.remove('overlay--isOpen--forRightSidebar')
         })
+    })
+    overlay.addEventListener('click',()=>{
+        sidebarCall.classList.remove('sidebar--isOpen')
+        sidebarChat.classList.remove('sidebar--isOpen')
+        overlay.classList.remove('overlay--isOpen--forRightSidebar')
+        if (window.innerWidth<1200){
+            sidebarBurger.classList.remove('sidebar--isOpen')
+            overlay.classList.remove('overlay--isOpen--forLeftSidebar')
+        }
     })
 }
