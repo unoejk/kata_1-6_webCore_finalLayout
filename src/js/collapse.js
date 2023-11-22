689e245ab8bc85c992bb11114aa15ffc338712ba
Leonid Burtsev <unoejk@gmail.com>
2023-11-22 09:18:59 +0300

    3try

--- src/js/collapse.js
+++ src/js/collapse.js
@@ -12,23 +12,28 @@ export const useCollapse=()=>{
     openMainManifesto.addEventListener('click',()=>{
         if (mainManifesto.classList.contains('collapsingBlock--isOpen')){
             mainManifesto.classList.remove('collapsingBlock--isOpen')
+            openMainManifesto.textContent='Читать далее'
         }else {
             mainManifesto.classList.add('collapsingBlock--isOpen')
+            openMainManifesto.textContent='Свернуть'
         }
     })
     openRepairBrandBlock.addEventListener('click',()=>{
-        console.log(123)
         if (repairBrandBlock.classList.contains('collapsingBlock--isOpen')){
             repairBrandBlock.classList.remove('collapsingBlock--isOpen')
+            openRepairBrandBlock.textContent='Показать все'
         }else {
             repairBrandBlock.classList.add('collapsingBlock--isOpen')
+            openRepairBrandBlock.textContent='Свернуть'
         }
     })
     openRepairTechBlock.addEventListener('click',()=>{
         if (repairTechBlock.classList.contains('collapsingBlock--isOpen')){
             repairTechBlock.classList.remove('collapsingBlock--isOpen')
+            openRepairTechBlock.textContent='Показать все'
         }else {
             repairTechBlock.classList.add('collapsingBlock--isOpen')
+            openRepairTechBlock.textContent='Свернуть'
         }
     })
 }
